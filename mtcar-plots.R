48f60570b34b7a89ad03e2660f054409c0c3cd70
pcsinu <pcsinu89@gmail.com>
2019-06-23 21:30:11 +0530

    Add files via upload

---
+++ mtcar-plots.R
@@ -0,0 +1,12 @@
+#mpg , cyl ,disp , hp , drat , wt ,qsec , vs , am , gear ,carb
+hist(mtcars$mpg)
+hist(mtcars$cyl)
+hist(mtcars$hp)
+hist(mtcars$drat)
+hist(mtcars$wt)
+hist(mtcars$qsec)
+hist(mtcars$vs)
+hist(mtcars$am)
+hist(mtcars$gear)
+hist(mtcars$carb)
+
